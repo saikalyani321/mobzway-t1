62a33d16781274f311e97bcd23fb3f16a729ae69
saikalyani321 <143479195+saikalyani321@users.noreply.github.com>
2024-05-21 17:46:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello world</h1>');
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
